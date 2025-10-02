8d85457a5f88af5151039104425932e6ea242545
yumauda <firststep-yumauda@gmail.com>
2025-10-02 16:45:40 +0900

    footerのコーディング

--- footer.php
+++ footer.php
@@ -1,6 +1,32 @@
-<footer class="l-footer p-footer">
+<footer class="p-footer">
     <div class="l-inner">
-        footer.php
+        <figure class="p-footer__img">
+            <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/footer_img.png" alt="エントリーはこちらから" width="520" height="250">
+        </figure>
+        <div class="p-footer__btn-wrapper">
+            <a href="#" class="p-footer__btn">
+                <div class="p-footer__btn-icon">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/footer_icon.png" alt="" width="24" height="24">
+                </div>
+                <div class="p-footer__btn-text">
+                    <p class="p-footer__btn-text-ja">募集要項</p>
+                    <p class="p-footer__btn-text-en">recruit</p>
+                </div>
+            </a>
+            <a href="#" class="p-footer__btn">
+                <div class="p-footer__btn-icon">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/footer_icon.png" alt="" width="24" height="24">
+                </div>
+                <div class="p-footer__btn-text">
+                    <p class="p-footer__btn-text-ja">エントリー</p>
+                    <p class="p-footer__btn-text-en">ENTRY</p>
+                </div>
+            </a>
+        </div>
+        <div class="p-footer__small">
+            <small>© 2025 YAMAGAMI CO.,LTD.</small>
+        </div>
+        
     </div>
 </footer>
 <?php wp_footer(); ?>
