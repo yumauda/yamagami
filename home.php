ba1c706302311c247f347923da2e01561195dad5
yumauda <firststep-yumauda@gmail.com>
2025-10-03 16:42:02 +0900

    先輩を知る一覧ページ構築

--- home.php
+++ home.php
@@ -1,7 +1,133 @@
 <?php get_header(); ?>
-<main>
-  <figure class="dummy">
+<main class="p-bg--interview">
+  <!-- <figure class="dummy">
     <img src="<?php echo get_template_directory_uri() ?>/images/common/home_pc.jpg" alt="">
-  </figure>
+  </figure> -->
+  <section class="p-page-mv">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <figure class="p-page-mv__img p-page-mv__img--interview">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/page_mv_interview.png" alt="先輩を知る" width="1473" height="562">
+        </figure>
+        <div class="p-page-mv__detail">
+          <h2 class="p-page-mv__title">先輩を知る</h2>
+          <p class="p-page-mv__en">What kind of company is Yamagami?</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="p-interview">
+    <div class="l-inner">
+      <div class="p-interview__content">
+        <div class="p-interview__title">
+          <h3 class="c-section-title">総合職(営業)</h3>
+        </div>
+        <ul class="p-interview__lists">
+          <li class="p-interview__list">
+            <a href="" class="p-interview__link">
+              <figure class="p-interview__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/interview_img1.jpg" alt="<?php the_title(); ?>" width="343" height="474">
+              </figure>
+              <div class="p-interview__detail">
+                <p class="p-interview__year">2025年入社</p>
+                <div class="p-interview__row">
+                  <p class="p-interview__name">山田 太郎</p>
+                  <p class="p-interview__name-en">Taro<br>Yamada</p>
+                </div>
+                <p class="p-interview__occupation">総合職（営業）</p>
+              </div>
+            </a>
+          </li>
+          <li class="p-interview__list">
+            <a href="" class="p-interview__link">
+              <figure class="p-interview__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/interview_img1.jpg" alt="<?php the_title(); ?>" width="343" height="474">
+              </figure>
+              <div class="p-interview__detail">
+                <p class="p-interview__year">2025年入社</p>
+                <div class="p-interview__row">
+                  <p class="p-interview__name">山田 太郎</p>
+                  <p class="p-interview__name-en">Taro<br>Yamada</p>
+                </div>
+                <p class="p-interview__occupation">総合職（営業）</p>
+              </div>
+            </a>
+          </li>
+          <li class="p-interview__list">
+            <a href="" class="p-interview__link">
+              <figure class="p-interview__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/interview_img1.jpg" alt="<?php the_title(); ?>" width="343" height="474">
+              </figure>
+              <div class="p-interview__detail">
+                <p class="p-interview__year">2025年入社</p>
+                <div class="p-interview__row">
+                  <p class="p-interview__name">山田 太郎</p>
+                  <p class="p-interview__name-en">Taro<br>Yamada</p>
+                </div>
+                <p class="p-interview__occupation">総合職（営業）</p>
+              </div>
+            </a>
+          </li>
+          <li class="p-interview__list">
+            <a href="" class="p-interview__link">
+              <figure class="p-interview__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/interview_img1.jpg" alt="<?php the_title(); ?>" width="343" height="474">
+              </figure>
+              <div class="p-interview__detail">
+                <p class="p-interview__year">2025年入社</p>
+                <div class="p-interview__row">
+                  <p class="p-interview__name">山田 太郎</p>
+                  <p class="p-interview__name-en">Taro<br>Yamada</p>
+                </div>
+                <p class="p-interview__occupation">総合職（営業）</p>
+              </div>
+            </a>
+          </li>
+        </ul>
+        <div class="p-interview__bottom-row">
+          <div class="p-interview__block">
+            <div class="p-interview__title">
+              <h3 class="c-section-title">一般職(事務)</h3>
+            </div>
+            <div class="p-interview__single">
+              <a href="" class="p-interview__link">
+                <figure class="p-interview__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/interview_img1.jpg" alt="<?php the_title(); ?>" width="343" height="474">
+                </figure>
+                <div class="p-interview__detail">
+                  <p class="p-interview__year">2025年入社</p>
+                  <div class="p-interview__row">
+                    <p class="p-interview__name">山田 太郎</p>
+                    <p class="p-interview__name-en">Taro<br>Yamada</p>
+                  </div>
+                  <p class="p-interview__occupation">総合職（営業）</p>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="p-interview__block">
+            <div class="p-interview__title">
+              <h3 class="c-section-title">一般職(事務)</h3>
+            </div>
+            <div class="p-interview__single">
+              <a href="" class="p-interview__link">
+                <figure class="p-interview__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/interview_img1.jpg" alt="<?php the_title(); ?>" width="343" height="474">
+                </figure>
+                <div class="p-interview__detail">
+                  <p class="p-interview__year">2025年入社</p>
+                  <div class="p-interview__row">
+                    <p class="p-interview__name">山田 太郎</p>
+                    <p class="p-interview__name-en">Taro<br>Yamada</p>
+                  </div>
+                  <p class="p-interview__occupation">総合職（営業）</p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </main>
 <?php get_footer() ?>
