c62cc28e45904bf3f7fb595cde9146715ae00d3b
yumauda <firststep-yumauda@gmail.com>
2025-10-03 18:22:02 +0900

    socialページ6割

--- page-social.php
+++ page-social.php
@@ -1,7 +1,113 @@
 <?php get_header(); ?>
-<main>
-  <figure class="dummy">
+<main class="p-bg--social">
+  <!-- <figure class="dummy">
     <img src="<?php echo get_template_directory_uri() ?>/images/common/social_pc.jpg" alt="">
-  </figure>
+  </figure> -->
+  <section class="p-page-mv">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <figure class="p-page-mv__img p-page-mv__img--social">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/page_mv_social.png" alt="社会貢献活動" width="1473" height="562">
+        </figure>
+        <div class="p-page-mv__detail">
+          <h2 class="p-page-mv__title">社会貢献活動</h2>
+          <p class="p-page-mv__en">Benefits & Workplace Culture</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="p-social">
+    <div class="l-inner">
+      <div class="p-social__content">
+        <div class="p-social__title">
+          <h3 class="c-section-title">社会貢献</h3>
+        </div>
+        <div class="p-social__top">
+          <h4 class="c-subTitle">
+            <span class="c-subTitle__en">Our<br>Initiatives</span>
+            <span class="c-subTitle__ja">私たちの取り込み</span>
+          </h4>
+          <div class="p-social__top-right">
+            <p class="p-social__top-title">社会に貢献し<br>社員と共に成長する取り組み</p>
+            <p class="p-social__top-en">Initiatives that contribute to society<br>and foster growth together with our employees.</p>
+          </div>
+        </div>
+        <div class="p-social__row">
+          <div class="p-social__block" data-number="01">
+            <div class="p-social__block-left">
+              <p class="p-social__block-title">sdgs</p>
+              <figure class="p-social__block-img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/social_logo.png" alt="sdgs" width="200" height="72">
+              </figure>
+            </div>
+            <ul class="p-social__lists">
+              <li class="p-social__list">ジェンダーや年齢に捉われず、全ての従業員が平等に活躍できる企業を目指しています。</li>
+              <li class="p-social__list">リサイクル商材をより多く販売し、CO2削減など、環境への負担を低減する取り組みを推進しています。</li>
+            </ul>
+
+          </div>
+          <div class="p-social__block" data-number="02">
+            <div class="p-social__block-left">
+              <p class="p-social__block-ja">健康経営<br>優良法人</p>
+              <figure class="p-social__block-img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/social_logo2.png" alt="健康経営優良法人" width="200" height="72">
+              </figure>
+            </div>
+            <div class="p-social__lists">
+              <p class="p-social__list">従業員の健康管理や健康増進の取り組みについて、特に優良と認められた法人</p>
+              <p class="p-social__bigText">5年連続取得中</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="p-local">
+    <div class="l-inner">
+      <div class="p-local__content">
+        <div class="p-local__title">
+          <h3 class="c-section-title c-section-title--white">地域の応援活動</h3>
+        </div>
+        <div class="p-local__top">
+          <h4 class="c-subTitle">
+            <span class="c-subTitle__en c-subTitle__en--white">Our<br>Initiatives</span>
+            <span class="c-subTitle__ja c-subTitle__ja--white">私たちの取り込み</span>
+          </h4>
+          <div class="p-local__top-right">
+            <p class="p-local__top-title p-local__top-title--white">スポーツを通じて、<br>地域の未来を応援します</p>
+            <p class="p-local__top-en p-local__top-en--white">Supporting the future of our community<br>through sports.</p>
+          </div>
+        </div>
+        <div class="p-local__row">
+          <div class="p-local__block" data-number="01">
+            <div class="p-local__block-left">
+              <p class="p-local__block-title">バスケット<br>ゴール寄贈</p>
+
+            </div>
+            <div class="p-local__text-wrapper">
+              <p class="p-local__text">神戸市と協力し、バスケットボールの振興および地域活性化を目指しています。</p>
+              <div class="p-local__flex">
+                <p class="p-local__before"><span>52</span>カ所</p>
+                <div class="p-local__arrow">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/local_arrow.png" alt="arrow" width="25" height="25">
+                </div>
+                <p class="p-local__after"><span>100</span>カ所</p>
+              </div>
+            </div>
+
+          </div>
+          <div class="p-local__block" data-number="02">
+            <div class="p-local__block-left">
+              <p class="p-local__block-title">バスケット<br>ボール寄贈</p>
+            </div>
+            <div class="p-local__text-wrapper">
+              <p class="p-local__text">地域社会貢献と、スポーツ振興のために、兵庫県内の中学校と高校にバスケットボールの寄贈を行っています。</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </main>
 <?php get_footer() ?>
