bade92cc62b084fa1c2e3eaefc754db24c0ff2fc
yumauda <firststep-yumauda@gmail.com>
2025-10-03 22:09:32 +0900

    socialページPC

--- page-social.php
+++ page-social.php
@@ -109,5 +109,102 @@
       </div>
     </div>
   </section>
+  <section class="p-social mt40">
+    <div class="l-inner">
+      <div class="p-social__content">
+        <div class="p-social__title">
+          <h3 class="c-section-title c-section-title--storks">KOBE STORKS</h3>
+        </div>
+        <div class="p-social__top">
+          <h4 class="c-subTitle">
+            <span class="c-subTitle__en">Our<br>Initiatives</span>
+            <span class="c-subTitle__ja">私たちの取り込み</span>
+          </h4>
+          <div class="p-social__top-right">
+            <p class="p-social__top-title">Yamagamiは<br>KOBE STORKSを応援してます</p>
+            <p class="p-social__top-en">Yamagami supports KOBE STORKS.</p>
+          </div>
+        </div>
+        <div class="p-social__storks p-storks">
+          <figure class="p-storks__img">
+            <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/storks_img.jpg" alt="KOBE STORKS" width="431" height="382">
+          </figure>
+          <div class="p-storks__detail">
+            <figure class="p-storks__logo">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/storks_logo.png" alt="official logo" width="600" height="117">
+            </figure>
+            <p class="p-storks__text">スポーツの力で地域に活気をもたらすと同時に、<br>社員や家族、お客様との交流の機会にもつなげています。</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="p-know">
+    <div class="l-inner">
+      <div class="p-know__content">
+        <div class="p-know__left">
+          <div class="p-know__title">
+            <h3 class="c-section-title">働く環境を知る</h3>
+          </div>
+          <div class="p-know__challenge-wrapper">
+            <p class="p-know__challenge">挑戦を続けられる</p>
+            <p class="p-know__challenge">環境がある。</p>
+          </div>
+          <p class="p-know__text">社員一人ひとりが自らの能力を高めることを前提として、それを支援するための環境を整備し、人材育成を図っています。</p>
+          <p class="p-know__white">Yamagami is committed to developing its human resources by providing an environment that includes an educational system and benefits package that supports each employee in improving their own capabilities.</p>
+        </div>
+        <ul class="p-know__lists">
+          <li class="p-know__list">
+            <a href="#" class="p-know__link">
+              <figure class="p-know__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/know_img1.png" alt="福利厚生・社内制度" width="300" height="410">
+              </figure>
+              <div class="p-know__detail">
+                <div class="p-know__detail-titleWrapper">
+                  <p class="p-know__detail-title">福利厚生・社内制度</p>
+                  <p class="p-know__detail-en">support</p>
+                </div>
+                <div class="p-know__icon">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/know_icon.png" alt="" width="60" height="60">
+                </div>
+              </div>
+            </a>
+          </li>
+          <li class="p-know__list">
+            <a href="#" class="p-know__link">
+              <figure class="p-know__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/know_img2.png" alt="成長できる環境" width="300" height="410">
+              </figure>
+              <div class="p-know__detail">
+                <div class="p-know__detail-titleWrapper">
+                  <p class="p-know__detail-title">成長できる環境</p>
+                  <p class="p-know__detail-en">growth</p>
+                </div>
+                <div class="p-know__icon">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/know_icon.png" alt="" width="60" height="60">
+                </div>
+              </div>
+            </a>
+          </li>
+          <li class="p-know__list">
+            <a href="#" class="p-know__link">
+              <figure class="p-know__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/know_img3.png" alt="福利厚生・社内制度" width="300" height="410">
+              </figure>
+              <div class="p-know__detail">
+                <div class="p-know__detail-titleWrapper">
+                  <p class="p-know__detail-title">社会貢献活動</p>
+                  <p class="p-know__detail-en">social</p>
+                </div>
+                <div class="p-know__icon">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/know_icon.png" alt="" width="60" height="60">
+                </div>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
 </main>
 <?php get_footer() ?>
