1d667f2a5e86777f66aa15c049b7197d461267d4
yumauda <firststep-yumauda@gmail.com>
2025-10-04 11:17:48 +0900

    福利厚生・社内制度ページPC

--- page-support.php
+++ page-support.php
@@ -1,7 +1,263 @@
 <?php get_header(); ?>
 <main>
-  <figure class="dummy">
+  <!--  <figure class="dummy">
     <img src="<?php echo get_template_directory_uri() ?>/images/common/support_pc.jpg" alt="">
-  </figure>
+  </figure> -->
+  <?php get_header(); ?>
+  <main class="p-bg--growth">
+    <!-- <figure class="dummy">
+    <img src="<?php echo get_template_directory_uri() ?>/images/common/social_pc.jpg" alt="">
+  </figure> -->
+    <section class="p-page-mv">
+      <div class="l-inner">
+        <div class="p-page-mv__content">
+          <figure class="p-page-mv__img p-page-mv__img--social">
+            <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/page_mv_support.png" alt="福利厚生・社内制度" width="1473" height="562">
+          </figure>
+          <div class="p-page-mv__detail">
+            <h2 class="p-page-mv__title">福利厚生・社内制度</h2>
+            <p class="p-page-mv__en">Benefits & Workplace Culture</p>
+          </div>
+          <div class="p-page-mv__btn-wrapper">
+            <a href="#" class="p-page-mv__btn">
+              <div class="p-page-mv__btn-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+                  <g transform="translate(-1465 -60)">
+                    <g id="arrow-right" transform="translate(1501.392 53.608) rotate(90)">
+                      <path d="M8.913,21.392H33.871" fill="none" stroke="#7cf513" stroke-linejoin="round" stroke-width="3.565" />
+                      <path d="M21.392,8.913,33.871,21.392,21.392,33.871" fill="none" stroke="#7cf513" stroke-width="3.565" />
+                    </g>
+                    <rect id="AdobeStock_1544788716" width="30" height="30" transform="translate(1495 60) rotate(90)" fill="none" />
+                  </g>
+                </svg>
+              </div>
+              <p class="p-page-mv__btn-text">賞与・昇給</p>
+            </a>
+            <a href="#" class="p-page-mv__btn">
+              <div class="p-page-mv__btn-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+                  <g transform="translate(-1465 -60)">
+                    <g id="arrow-right" transform="translate(1501.392 53.608) rotate(90)">
+                      <path d="M8.913,21.392H33.871" fill="none" stroke="#7cf513" stroke-linejoin="round" stroke-width="3.565" />
+                      <path d="M21.392,8.913,33.871,21.392,21.392,33.871" fill="none" stroke="#7cf513" stroke-width="3.565" />
+                    </g>
+                    <rect id="AdobeStock_1544788716" width="30" height="30" transform="translate(1495 60) rotate(90)" fill="none" />
+                  </g>
+                </svg>
+              </div>
+              <p class="p-page-mv__btn-text">安心の社宅制度</p>
+            </a>
+            <a href="#" class="p-page-mv__btn">
+              <div class="p-page-mv__btn-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+                  <g transform="translate(-1465 -60)">
+                    <g id="arrow-right" transform="translate(1501.392 53.608) rotate(90)">
+                      <path d="M8.913,21.392H33.871" fill="none" stroke="#7cf513" stroke-linejoin="round" stroke-width="3.565" />
+                      <path d="M21.392,8.913,33.871,21.392,21.392,33.871" fill="none" stroke="#7cf513" stroke-width="3.565" />
+                    </g>
+                    <rect id="AdobeStock_1544788716" width="30" height="30" transform="translate(1495 60) rotate(90)" fill="none" />
+                  </g>
+                </svg>
+              </div>
+              <p class="p-page-mv__btn-text">手当</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="p-social">
+      <div class="l-inner">
+        <div class="p-social__content">
+          <div class="p-social__title">
+            <h3 class="c-section-title">賞与・昇給</h3>
+          </div>
+          <div class="p-social__top">
+            <h4 class="c-subTitle">
+              <span class="c-subTitle__en">Bonuses &<br>Salary<br>Increases</span>
+              <span class="c-subTitle__ja">ボーナスと給与昇給</span>
+            </h4>
+            <div class="p-social__top-right">
+              <p class="p-social__top-title">あなたの日々の頑張りが<br>きちんと評価され、<span>カタチ</span>になる</p>
+              <p class="p-social__top-en">Your daily hard work<br>is properly recognized and takes shape.</p>
+            </div>
+          </div>
+          <div class="p-social__salary">
+            <div class="p-salary">
+              <figure class="p-salary__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/salary_img1.png" alt="賞与" width="520" height="450">
+              </figure>
+              <figure class="p-salary__img p-salary__img--center">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/salary_img2.png" alt="" width="520" height="358">
+              </figure>
+              <figure class="p-salary__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/salary_img3.png" alt="昇給" width="520" height="450">
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="p-event">
+      <div class="l-inner">
+        <div class="p-event__content">
+          <div class="p-event__title">
+            <h3 class="c-section-title c-section-title--white">安心の社宅制度</h3>
+          </div>
+          <div class="p-event__top">
+            <h4 class="c-subTitle">
+              <span class="c-subTitle__en c-subTitle__en--white">Company<br>Housing<br>Support</span>
+              <span class="c-subTitle__ja c-subTitle__ja--white">会社・住宅サポート</span>
+            </h4>
+            <div class="p-event__top-right">
+              <p class="p-event__top-title p-event__top-title--white">経済的な負担を軽減し、<br><span>仕事に集中できる</span>環境を</p>
+              <p class="p-event__top-en p-event__top-en--white">An environment that reduces financial burdens<br>and allows you to focus on your work.</p>
+            </div>
+          </div>
+          <figure class="p-event__img">
+            <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/event_environment.png" alt="仕事に集中できる環境" width="1600" height="500">
+          </figure>
+          <div class="p-event__support">
+            <div class="p-support">
+              <ul class="p-support__lists">
+                <li class="p-support__list">家賃1万円で社宅に住める</li>
+                <li class="p-support__list">通勤2時間以上の社員が対象</li>
+                <li class="p-support__list">給与から天引きだから所得税も増えない</li>
+              </ul>
+              <div class="p-support__right">
+                <p class="p-support__title">※27歳の4月まで</p>
+                <p class="p-support__small">短大卒で20歳から利用すると最大7年間</p>
+                <div class="p-support__attention">
+                  <p class="p-support__attention-text">※その後は1年毎に1万円ずつ増額</p>
+                  <p class="p-support__attention-text">※社内規定あり</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="p-social">
+      <div class="l-inner">
+        <div class="p-social__content">
+          <div class="p-social__title">
+            <h3 class="c-section-title">手当</h3>
+          </div>
+          <div class="p-social__top">
+            <h4 class="c-subTitle">
+              <span class="c-subTitle__en">Allowances<br>&<br>Benefits</span>
+              <span class="c-subTitle__ja">手当制度</span>
+            </h4>
+            <div class="p-social__top-right">
+              <p class="p-social__top-title">安心して働ける<br>充実の<span>手当とサポート体制</span>を完備</p>
+              <p class="p-social__top-en">Your daily hard work<br>is properly recognized and takes shape.</p>
+            </div>
+          </div>
+          <div class="p-social__benefits">
+            <ul class="p-benefits">
+              <li class="p-benefits__list">
+                <p class="p-benefits__number">01</p>
+                <p class="p-benefits__title">固定残業手当</p>
+              </li>
+              <li class="p-benefits__list">
+                <p class="p-benefits__number">02</p>
+                <div class="p-benefits__column">
+                  <p class="p-benefits__title">固定残業手当</p>
+                  <p class="p-benefits__small">１分単位で支給</p>
+                </div>
+              </li>
+              <li class="p-benefits__list">
+                <p class="p-benefits__number">03</p>
+                <div class="p-benefits__column">
+                  <p class="p-benefits__title">家族手当</p>
+                  <p class="p-benefits__small">配偶者１万円／こども５千円</p>
+                </div>
+              </li>
+              <li class="p-benefits__list">
+                <p class="p-benefits__number">04</p>
+                <p class="p-benefits__title">資格手当</p>
+              </li>
+              <li class="p-benefits__list">
+                <p class="p-benefits__number">05</p>
+                <p class="p-benefits__title">役職手当</p>
+              </li>
+              <li class="p-benefits__list">
+                <p class="p-benefits__number">06</p>
+                <p class="p-benefits__title">単身赴任手当</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="p-know">
+      <div class="l-inner">
+        <div class="p-know__content">
+          <div class="p-know__left">
+            <div class="p-know__title">
+              <h3 class="c-section-title">働く環境を知る</h3>
+            </div>
+            <div class="p-know__challenge-wrapper">
+              <p class="p-know__challenge">挑戦を続けられる</p>
+              <p class="p-know__challenge">環境がある。</p>
+            </div>
+            <p class="p-know__text">社員一人ひとりが自らの能力を高めることを前提として、それを支援するための環境を整備し、人材育成を図っています。</p>
+            <p class="p-know__white">Yamagami is committed to developing its human resources by providing an environment that includes an educational system and benefits package that supports each employee in improving their own capabilities.</p>
+          </div>
+          <ul class="p-know__lists">
+            <li class="p-know__list">
+              <a href="#" class="p-know__link">
+                <figure class="p-know__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/know_img1.png" alt="福利厚生・社内制度" width="300" height="410">
+                </figure>
+                <div class="p-know__detail">
+                  <div class="p-know__detail-titleWrapper">
+                    <p class="p-know__detail-title">福利厚生・社内制度</p>
+                    <p class="p-know__detail-en">support</p>
+                  </div>
+                  <div class="p-know__icon">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/know_icon.png" alt="" width="60" height="60">
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li class="p-know__list">
+              <a href="#" class="p-know__link">
+                <figure class="p-know__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/know_img2.png" alt="成長できる環境" width="300" height="410">
+                </figure>
+                <div class="p-know__detail">
+                  <div class="p-know__detail-titleWrapper">
+                    <p class="p-know__detail-title">成長できる環境</p>
+                    <p class="p-know__detail-en">growth</p>
+                  </div>
+                  <div class="p-know__icon">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/know_icon.png" alt="" width="60" height="60">
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li class="p-know__list">
+              <a href="#" class="p-know__link">
+                <figure class="p-know__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/know_img3.png" alt="福利厚生・社内制度" width="300" height="410">
+                </figure>
+                <div class="p-know__detail">
+                  <div class="p-know__detail-titleWrapper">
+                    <p class="p-know__detail-title">社会貢献活動</p>
+                    <p class="p-know__detail-en">social</p>
+                  </div>
+                  <div class="p-know__icon">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/know_icon.png" alt="" width="60" height="60">
+                  </div>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </main>
+  <?php get_footer() ?>
 </main>
 <?php get_footer() ?>
